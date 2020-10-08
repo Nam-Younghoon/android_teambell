16f07e240324b5db8644baa27818133e1b77262b
huny3410@naver.com <dudgns0504>
2020-10-08 15:33:38 +0900

    통계 ViewPager

--- app/src/main/java/com/example/teambell_3/Statistic_Fragment.java
+++ app/src/main/java/com/example/teambell_3/Statistic_Fragment.java
@@ -4,17 +4,23 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
 import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
 import androidx.fragment.app.FragmentTransaction;
+import androidx.viewpager.widget.ViewPager;
 
 public class Statistic_Fragment extends Fragment {
 
-    private Button dayStat, weekStat, monthStat, yearStat;
+    private TextView dayStat, weekStat, monthStat, yearStat;
+    ViewPager vp;
+    LinearLayout ll;
 
     @Nullable
     @Override
@@ -22,44 +28,60 @@ public class Statistic_Fragment extends Fragment {
         setHasOptionsMenu(true);
         View v = inflater.inflate(R.layout.statistic_fragment, container, false);
 
-        getChildFragmentManager().beginTransaction().add(R.id.child_fragment, new Stat_Day()).commit();
-
         dayStat = v.findViewById(R.id.button_day);
         weekStat = v.findViewById(R.id.button_week);
         monthStat = v.findViewById(R.id.button_month);
         yearStat = v.findViewById(R.id.button_year);
+        vp = v.findViewById(R.id.vp);
+        ll = v.findViewById(R.id.ll);
+
+        vp.setAdapter(new pagerAdapter(getChildFragmentManager()));
+        vp.setCurrentItem(0);
 
 
-        dayStat.setOnClickListener(new View.OnClickListener() {
+        dayStat.setOnClickListener(movePageListener);
+        dayStat.setTag(0);
+
+        weekStat.setOnClickListener(movePageListener);
+        weekStat.setTag(1);
+
+        monthStat.setOnClickListener(movePageListener);
+        monthStat.setTag(2);
+
+        yearStat.setOnClickListener(movePageListener);
+        yearStat.setTag(3);
+
+        dayStat.setSelected(true);
+
+        vp.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
             @Override
-            public void onClick(View v) {
-                getChildFragmentManager().beginTransaction().replace(R.id.child_fragment, new Stat_Day()).commit();
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
             }
-        });
 
-        weekStat.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                getChildFragmentManager().beginTransaction().replace(R.id.child_fragment, new Stat_Week()).commit();
+            public void onPageSelected(int position) {
+                int i = 0;
+                while (i<4){
+                    if(position == i){
+                        ll.findViewWithTag(i).setSelected(true);
+                    }
+                    else {
+                        ll.findViewWithTag(i).setSelected(false);
+                    }
+                    i++;
+                }
             }
-        });
 
-        monthStat.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                getChildFragmentManager().beginTransaction().replace(R.id.child_fragment, new Stat_Month()).commit();
-            }
-        });
+            public void onPageScrollStateChanged(int state) {
 
-        yearStat.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                getChildFragmentManager().beginTransaction().replace(R.id.child_fragment, new Stat_Year()).commit();
             }
         });
 
         return v;
     }
+
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
@@ -71,6 +93,52 @@ public class Statistic_Fragment extends Fragment {
         FragmentActivity activity = getActivity();
         if(activity != null){
             ((MainActivity) activity).setActionBarTitle(R.string.title_statistic);
+        }
+    }
+
+    View.OnClickListener movePageListener = new View.OnClickListener(){
+        @Override
+        public void onClick(View v) {
+            int tag = (int) v.getTag();
+            int i = 0;
+            while(i<4){
+                if(tag == i){
+                    ll.findViewWithTag(i).setSelected(true);
+                }
+                else {
+                    ll.findViewWithTag(i).setSelected(false);
+                }
+                i++;
+            }
+            vp.setCurrentItem(tag);
+        }
+    };
+
+    private class pagerAdapter extends FragmentStatePagerAdapter{
+        public pagerAdapter(FragmentManager fm){
+            super(fm);
+        }
+
+        @NonNull
+        @Override
+        public Fragment getItem(int position) {
+            switch (position){
+                case 0:
+                    return new Stat_Day();
+                case 1:
+                    return new Stat_Week();
+                case 2:
+                    return new Stat_Month();
+                case 3:
+                    return new Stat_Year();
+                default:
+                    return null;
+            }
+        }
+
+        @Override
+        public int getCount() {
+            return 4;
         }
     }
 }
